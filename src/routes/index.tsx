387aa98b6b6ebb0ff21677babeb208cb3ff1054a
Mike Kenway <mike@mikekenway.com>
2025-09-06 14:57:12 -0400

    styling tweaks

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -27,7 +27,7 @@ function Index() {
                 variant='outline'
                 className='bg-primary/10 text-primary border-primary/30 px-4 py-2'
               >
-                <Zap className='w-4 h-4 mr-2' />
+                <Zap className='size-4 mr-2' />
                 App Started Successfully
               </Badge>
             </div>
@@ -46,7 +46,7 @@ function Index() {
             <Card className='bg-card/50 border-border/50 backdrop-blur-sm shadow-lg'>
               <CardHeader className='pb-6'>
                 <CardTitle className='flex items-center justify-center gap-2 text-xl'>
-                  <Code2 className='w-6 h-6 text-primary' />
+                  <Code2 className='size-6 text-primary' />
                   Interactive Demo
                 </CardTitle>
               </CardHeader>
@@ -60,9 +60,9 @@ function Index() {
                     variant='outline'
                     size='lg'
                     onClick={() => setCount(count - 1)}
-                    className='w-14 h-14 p-0 hover:scale-105 transition-transform'
+                    className='size-14 p-0 hover:scale-105 transition-transform'
                   >
-                    <Minus className='w-5 h-5' />
+                    <Minus className='size-5' />
                   </Button>
                   <Button
                     variant='outline'
@@ -70,16 +70,16 @@ function Index() {
                     onClick={() => setCount(0)}
                     className='px-8 py-3 hover:scale-105 transition-transform'
                   >
-                    <RotateCcw className='w-5 h-5 mr-2' />
+                    <RotateCcw className='size-5 mr-2' />
                     Reset
                   </Button>
                   <Button
                     variant='outline'
                     size='lg'
                     onClick={() => setCount(count + 1)}
-                    className='w-14 h-14 p-0 hover:scale-105 transition-transform'
+                    className='size-14 p-0 hover:scale-105 transition-transform'
                   >
-                    <Plus className='w-5 h-5' />
+                    <Plus className='size-5' />
                   </Button>
                 </div>
               </CardContent>
